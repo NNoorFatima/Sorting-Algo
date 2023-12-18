9ea5f57d0b095017175c3eda5eb1370ad3bc9d9c
NNoorFatima <50588372+NNoorFatima@users.noreply.github.com>
2023-12-19 01:11:40 +0500

    Update insertion_sort.h

--- insertion_sort.h
+++ insertion_sort.h
@@ -1,5 +1,9 @@
 #include <iostream>
 using namespace std;
+
+/*If the data is nearly sorted or when the list is small as it has a complexity of O(N2) and if the list is sorted a minimum number of elements will slide over to insert the element at its correct location.
+This algorithm is stable and it has fast running case when the list is nearly sorted.
+The usage of memory is a constraint as it has space complexity of O(1).*/
 void insertionSort(int arr[], int size)
 {
 	int i=0,j=0;
